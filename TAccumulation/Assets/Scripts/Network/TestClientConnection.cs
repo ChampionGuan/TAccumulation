c2b283a8a2efc3512bd524ca4a680bd1b3cfc633
ChampionGuan <champion_guan@163.com>
2019-02-19 15:20:02 +0800

    network

---
+++ TAccumulation/Assets/Scripts/Network/TestClientConnection.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LCG
+{
+    public class TestClientConnection : MonoBehaviour
+    {
+        private ClientConnection clientHandle = new ClientConnection();
+
+
+        // Update is called once per frame
+        void Update()
+        {
+            byte[] msg = clientHandle.GetRecvMsg();
+            if (null != msg)
+            {
+                Debug.Log("客户端接受到消息：" + System.Text.Encoding.Default.GetString(msg));
+            }
+
+            if (Input.GetKeyDown(KeyCode.J))
+            {
+                clientHandle.Send(System.Text.Encoding.Default.GetBytes("来自客户端的消息"));
+            }
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                clientHandle.Connect("192.168.1.110", 60000, null);
+            }
+        }
+    }
+}
---
+++ TAccumulation/Assets/Scripts/Network/TestServerConnection.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LCG
+{
+    public class TestServerConnection : MonoBehaviour
+    {
+        private ServerConnection serverHandle = new ServerConnection();
+
+        void Start()
+        {
+            serverHandle.Accept("192.168.1.110", 60000);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            foreach (var v in serverHandle.Connections)
+            {
+                byte[] msg = v.GetRecvMsg();
+                if (null != msg)
+                {
+                    Debug.Log("服务器接受到消息：" + System.Text.Encoding.Default.GetString(msg));
+                }
+            }
+
+            if (Input.GetKeyDown(KeyCode.K))
+            {
+                serverHandle.Send(System.Text.Encoding.Default.GetBytes("来自服务器的消息"));
+            }
+        }
+    }
+}
