4a521dfd8ede9af75f878af011df5df325be54ad
ChampionGuan <champion_guan@163.com>
2021-02-20 15:32:39 +0800

    事件分发

---
+++ TAccumulation/Assets/Scripts/Signal/SignalBus.cs
@@ -0,0 +1,210 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+
+namespace PapeGames.X3
+{
+    public class SignalBus : Singleton<SignalBus>
+    {
+        private Dictionary<int, BaseSignal> m_SignalDict = null;
+
+        protected override void Init()
+        {
+            base.Init();
+            m_SignalDict = new Dictionary<int, BaseSignal>();
+        }
+
+        protected override void UnInit()
+        {
+            base.UnInit();
+            m_SignalDict.Clear();
+        }
+
+        public void Dispatch(int key)
+        {
+            GetSignal(key).Dispatch();
+        }
+
+        public void Dispatch<T>(int key, T param1)
+        {
+            GetSignal<T>(key).Dispatch(param1);
+        }
+
+        public void Dispatch<T1, T2>(int key, T1 param1, T2 param2)
+        {
+            GetSignal<T1, T2>(key).Dispatch(param1, param2);
+        }
+
+        public void Dispatch<T1, T2, T3>(int key, T1 param1, T2 param2, T3 param3)
+        {
+            GetSignal<T1, T2, T3>(key).Dispatch(param1, param2, param3);
+        }
+
+        public void Dispatch<T1, T2, T3, T4>(int key, T1 param1, T2 param2, T3 param3, T4 param4)
+        {
+            GetSignal<T1, T2, T3, T4>(key).Dispatch(param1, param2, param3, param4);
+        }
+
+        public void Dispatch<T1, T2, T3, T4, T5>(int key, T1 param1, T2 param2, T3 param3, T4 param4, T5 param5)
+        {
+            GetSignal<T1, T2, T3, T4, T5>(key).Dispatch(param1, param2, param3, param4, param5);
+        }
+
+        public void AddListener(int key, Action callfunc)
+        {
+            GetSignal(key).AddListener(callfunc);
+        }
+
+        public void AddListener<T>(int key, Action<T> callfunc)
+        {
+            GetSignal<T>(key).AddListener(callfunc);
+        }
+
+        public void AddListener<T1, T2>(int key, Action<T1, T2> callfunc)
+        {
+            GetSignal<T1, T2>(key).AddListener(callfunc);
+        }
+
+        public void AddListener<T1, T2, T3>(int key, Action<T1, T2, T3> callfunc)
+        {
+            GetSignal<T1, T2, T3>(key).AddListener(callfunc);
+        }
+
+        public void AddListener<T1, T2, T3, T4>(int key, Action<T1, T2, T3, T4> callfunc)
+        {
+            GetSignal<T1, T2, T3, T4>(key).AddListener(callfunc);
+        }
+
+        public void AddListener<T1, T2, T3, T4, T5>(int key, Action<T1, T2, T3, T4, T5> callfunc)
+        {
+            GetSignal<T1, T2, T3, T4, T5>(key).AddListener(callfunc);
+        }
+
+        public void AddOnce(int key, Action callfunc)
+        {
+            GetSignal(key).AddOnce(callfunc);
+        }
+
+        public void AddOnce<T>(int key, Action<T> callfunc)
+        {
+            GetSignal<T>(key).AddListener(callfunc);
+        }
+
+        public void AddOnce<T1, T2>(int key, Action<T1, T2> callfunc)
+        {
+            GetSignal<T1, T2>(key).AddOnce(callfunc);
+        }
+
+        public void AddOnce<T1, T2, T3>(int key, Action<T1, T2, T3> callfunc)
+        {
+            GetSignal<T1, T2, T3>(key).AddOnce(callfunc);
+        }
+
+        public void AddOnce<T1, T2, T3, T4>(int key, Action<T1, T2, T3, T4> callfunc)
+        {
+            GetSignal<T1, T2, T3, T4>(key).AddListener(callfunc);
+        }
+
+        public void AddOnce<T1, T2, T3, T4, T5>(int key, Action<T1, T2, T3, T4, T5> callfunc)
+        {
+            GetSignal<T1, T2, T3, T4, T5>(key).AddOnce(callfunc);
+        }
+
+        public void RemoveListener(int key, Action callfunc)
+        {
+            GetSignal(key).RemoveListener(callfunc);
+        }
+
+        public void RemoveListener<T>(int key, Action<T> callfunc)
+        {
+            GetSignal<T>(key).RemoveListener(callfunc);
+        }
+
+        public void RemoveListener<T1, T2>(int key, Action<T1, T2> callfunc)
+        {
+            GetSignal<T1, T2>(key).RemoveListener(callfunc);
+        }
+
+        public void RemoveListener<T1, T2, T3>(int key, Action<T1, T2, T3> callfunc)
+        {
+            GetSignal<T1, T2, T3>(key).RemoveListener(callfunc);
+        }
+
+        public void RemoveListener<T1, T2, T3, T4>(int key, Action<T1, T2, T3, T4> callfunc)
+        {
+            GetSignal<T1, T2, T3, T4>(key).RemoveListener(callfunc);
+        }
+
+        public void RemoveListener<T1, T2, T3, T4, T5>(int key, Action<T1, T2, T3, T4, T5> callfunc)
+        {
+            GetSignal<T1, T2, T3, T4, T5>(key).RemoveListener(callfunc);
+        }
+
+        public Signal GetSignal(int key)
+        {
+            BaseSignal singal;
+            if (!m_SignalDict.TryGetValue(key, out singal))
+            {
+                singal = new Signal();
+                m_SignalDict.Add(key, singal);
+            }
+            return (Signal)singal;
+        }
+
+        public Signal<T> GetSignal<T>(int key)
+        {
+            BaseSignal singal;
+            if (!m_SignalDict.TryGetValue(key, out singal))
+            {
+                singal = new Signal<T>();
+                m_SignalDict.Add(key, singal);
+            }
+            return (Signal<T>)singal;
+        }
+
+        public Signal<T1, T2> GetSignal<T1, T2>(int key)
+        {
+            BaseSignal singal;
+            if (!m_SignalDict.TryGetValue(key, out singal))
+            {
+                singal = new Signal<T1, T2>();
+                m_SignalDict.Add(key, singal);
+            }
+            return (Signal<T1, T2>)singal;
+        }
+
+        public Signal<T1, T2, T3> GetSignal<T1, T2, T3>(int key)
+        {
+            BaseSignal singal;
+            if (!m_SignalDict.TryGetValue(key, out singal))
+            {
+                singal = new Signal<T1, T2, T3>();
+                m_SignalDict.Add(key, singal);
+            }
+            return (Signal<T1, T2, T3>)singal;
+        }
+
+        public Signal<T1, T2, T3, T4> GetSignal<T1, T2, T3, T4>(int key)
+        {
+            BaseSignal singal;
+            if (!m_SignalDict.TryGetValue(key, out singal))
+            {
+                singal = new Signal<T1, T2, T3, T4>();
+                m_SignalDict.Add(key, singal);
+            }
+            return (Signal<T1, T2, T3, T4>)singal;
+        }
+
+        public Signal<T1, T2, T3, T4, T5> GetSignal<T1, T2, T3, T4, T5>(int key)
+        {
+            BaseSignal singal;
+            if (!m_SignalDict.TryGetValue(key, out singal))
+            {
+                singal = new Signal<T1, T2, T3, T4, T5>();
+                m_SignalDict.Add(key, singal);
+            }
+            return (Signal<T1, T2, T3, T4, T5>)singal;
+        }
+    }
+}
+
---
+++ TAccumulation/Assets/Scripts/Signal/Singleton.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+namespace PapeGames.X3
+{
+    /// <summary>
+    /// Singleton
+    /// Creator: Tungway
+    /// Create Date: 2019
+    /// Updater: Tungway
+    /// Last Update: 2019
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class Singleton<T> where T : class, new()
+    {
+        private static T s_Instance;
+        private static object s_LockObj = new object();
+
+        protected Singleton()
+        {
+        }
+
+        protected static void CreateInstance()
+        {
+            if (s_Instance != null) return;
+            lock (s_LockObj)
+            {
+                s_Instance = new T();
+            }
+            (s_Instance as Singleton<T>).Init();
+        }
+
+        public static void DestroyInstance()
+        {
+            if (s_Instance != null)
+            {
+                (s_Instance as Singleton<T>).UnInit();
+                s_Instance = null;
+            }
+        }
+
+        public static T Instance
+        {
+            get
+            {
+                if (s_Instance != null)
+                    return s_Instance;
+
+                CreateInstance();
+
+                return s_Instance;
+            }
+        }
+
+        protected virtual void Init()
+        {
+
+        }
+
+        protected virtual void UnInit()
+        {
+
+        }
+    }
+}
